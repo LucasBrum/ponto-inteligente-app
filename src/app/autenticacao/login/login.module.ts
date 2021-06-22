918f5e9d3f35d83ee44019c57d4771adea0a771d
Lucas Brum <brum.lucas@icloud.com>
2021-06-22 10:49:45 -0300

    Criando tela de login

--- src/app/autenticacao/login/login.module.ts
+++ src/app/autenticacao/login/login.module.ts
@@ -1,15 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './components';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms'; //Criar formulários reativos
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
+import { LoginComponent, LogarComponent } from './components';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    LogarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FlexLayoutModule
   ]
 })
 export class LoginModule { }
