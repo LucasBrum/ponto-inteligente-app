88a21f28cd2d8489f43f90b6fcdbdea2005174fd
Lucas Brum <brum.lucas@icloud.com>
2021-10-10 22:45:07 -0300

    Criando Lançamentos

--- src/app/funcionario/components/listagem/listagem.component.ts
+++ src/app/funcionario/components/listagem/listagem.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { DataSource } from "@angular/cdk/collections";
+import { MatTableDataSource } from '@angular/material/table';
+import { PageEvent, MatPaginatorModule } from '@angular/material/paginator';
+import {MatSortModule, MatSort} from '@angular/material/sort';
+import { Observable} from "rxjs/Observable";
+import  'rxjs/add/observable/of';
+
+import { LancamentoService, Lancamento } from "src/app/shared";
+
 
 @Component({
   selector: 'app-listagem',
@@ -6,10 +16,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listagem.component.css']
 })
 export class ListagemComponent implements OnInit {
+  dataSource: MatTableDataSource<Lancamento>;
+  colunas: string[] = ['data', 'tipo', 'localizacao'];
 
-  constructor() { }
+  constructor(
+      private lancamentoService: LancamentoService,
+      private snackBar: MatSnackBar
+  ){ }
 
-  ngOnInit(): void {
+  ngOnInit() {
+      this.lancamentoService.listarTodosLancamentos()
+          .subscribe(
+              data => {
+                  const lancamentos = data['data'] as Lancamento[];
+                  this.dataSource = new MatTableDataSource<Lancamento>(lancamentos)
+              },
+              err => {
+                  const msg: string = "Erro obtendo  lançamentos.";
+                  this.snackBar.open(msg, "Erro", { duration: 5000 });
+              }
+          );
   }
 
 }
